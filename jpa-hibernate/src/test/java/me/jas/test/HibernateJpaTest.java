29fe42a82ddc4c8c58e7a5bfbdbcfdc974b06099
Jas <suny0906@gmail.com>
2022-11-06 20:51:38 +0900

    add updateOrInsertTest()

--- jpa-hibernate/src/test/java/me/jas/test/HibernateJpaTest.java
+++ jpa-hibernate/src/test/java/me/jas/test/HibernateJpaTest.java
@@ -53,4 +53,24 @@ public class HibernateJpaTest {
         System.out.println(customer);
         transaction.commit();
     }
+
+    @Test
+    public void updateOrInsertTest() {
+        EntityManager entityManager = factory.createEntityManager();
+
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+        Customer customer = new Customer();
+
+        /*
+          若指定了主键:
+                   会先查询看是否有变化，如有变化则更新，无变化则不更新
+          若没有指定主键：
+                   执行插入
+         */
+        customer.setCustomerId(1L);
+        customer.setCustomerName("jason");
+        entityManager.merge(customer);
+        transaction.commit();
+    }
 }
