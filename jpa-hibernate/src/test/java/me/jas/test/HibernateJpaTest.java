92b6e1818b22ae18f9458a298289d6b3a47fef08
Jas <suny0906@gmail.com>
2022-11-06 20:12:31 +0900

    add jpa

---
+++ jpa-hibernate/src/test/java/me/jas/test/HibernateJpaTest.java
@@ -0,0 +1,32 @@
+package me.jas.test;
+
+import me.jas.pojo.Customer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class HibernateJpaTest {
+
+    EntityManagerFactory factory;
+
+    @BeforeEach
+    public void before_each() {
+        factory = Persistence.createEntityManagerFactory("hibernateJPA");
+    }
+
+    @Test
+    public void addTest() {
+        EntityManager entityManager = factory.createEntityManager();
+
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+        Customer customer = new Customer();
+        customer.setCustomerName("tom");
+        entityManager.persist(customer);
+        transaction.commit();
+    }
+}
